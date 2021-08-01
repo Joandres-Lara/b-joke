3574ccd021065b6d25997a03f78ae0238188280c
Joan Andrés Lara Mora <joandres52752lm@gmail.com>
2021-07-31 19:58:44 -0500

    PM2 heroku

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	apps: [
+		{
+			name: "b-joke",
+			script: "index.js",
+			instances: "max",
+			watch: ".",
+			env: {
+				NODE_ENV: "development",
+			},
+			env_production: {
+				NODE_ENV: "production",
+			},
+		},
+	],
+
+	deploy: {
+		production: {
+			user: "SSH_USERNAME",
+			host: "SSH_HOSTMACHINE",
+			ref: "origin/master",
+			repo: "GIT_REPOSITORY",
+			path: "DESTINATION_PATH",
+			"pre-deploy-local": "",
+			"post-deploy":
+			"npm install && pm2 reload ecosystem.config.js --env production",
+			"pre-setup": "",
+		},
+	},
+};
