3da68954e433280f78550cd36c891d086513d579
Joan Andrés Lara Mora <joandres52752lm@gmail.com>
2021-08-02 08:23:43 -0500

    Update `joke` command description

--- src/services/JokeApiService.js
+++ src/services/JokeApiService.js
@@ -6,6 +6,8 @@ export default class JokeApiService {
  /**
   *
   * @param {Eris.Client} bot
+  * @param {Express.Application} appExpress
+  * @param {Storage}
   */
  constructor(bot, appExpress, storage) {
   this.bot = bot;
@@ -30,6 +32,12 @@ export default class JokeApiService {
     this.storage.insert(new JokeApiJobSchedule(channel_id));
     return this.bot.createMessage(channel_id, "Te has suscrito, para que te envíemos, chiste diarios.");
    }
+  }, {
+   description: "A joke to make you laugh, type **!help joke** for mor options.",
+   fullDescription: `
+    Use **!joke** only for get a joke and make you **laugh**.
+    use **!joke subscribe**, for get all days to 9:00 am, a joke.
+   `
   });
  };
 }
