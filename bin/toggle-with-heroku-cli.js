09fad1f9fc8fe64936bcb6c85421c9ed1cd998a7
Joan Andrés Lara Mora <joandres52752lm@gmail.com>
2022-04-05 10:10:08 -0500

    Mejora del script toggle-with-heroku-cli

--- bin/toggle-with-heroku-cli.js
+++ bin/toggle-with-heroku-cli.js
@@ -1,35 +1,98 @@
 import { exec as legacyExec } from "child_process";
+import { endOfMonth, isAfter } from "date-fns";
 
-async function exec(command){
- return await(new Promise((resolve, reject) => {
+async function exec(command) {
+ return await new Promise((resolve, reject) => {
   legacyExec(command, (error, stout) => {
-   if(error) return reject(error);
+   if (error) return reject(error);
    return resolve(stout);
-  })
- }));
+  });
+ });
+}
+export default function ms({
+ years = 0,
+ days = 0,
+ hours = 0,
+ minutes = 0,
+ seconds = 0,
+}) {
+ const transformers = {
+  years: 1000 * 60 * 60 * 24 * 365,
+  days: 1000 * 60 * 60 * 24,
+  hours: 1000 * 60 * 60,
+  minutes: 1000 * 60,
+  seconds: 1000,
+  miliseconds: 1,
+ };
+
+ return (
+  seconds * transformers.seconds +
+  minutes * transformers.minutes +
+  hours * transformers.hours +
+  days * transformers.days +
+  years * transformers.years
+ );
 }
 
 const MATCH_STATUS_OFF = "No dynos on";
 
 (async () => {
- const stoutHerokuStatus = await exec("heroku ps");
- let result = "";
+ try {
+  let timerHerokuResponse;
+  /** @type {string} */
+  const stoutHerokuStatus = await Promise.race([
+   exec("heroku ps"),
+   new Promise((...[, reject]) => {
+    timerHerokuResponse = setTimeout(() => {
+     reject(
+      new Error(
+       "La petición ha tardado demasiado, revisa si la aplicación no necesita el token para iniciar sesión"
+      )
+     );
+    }, 10000);
+   }),
+  ]);
 
- if(stoutHerokuStatus.indexOf(MATCH_STATUS_OFF) !== -1){
-  try{
+  clearTimeout(timerHerokuResponse);
+
+  let result = "";
+
+  const [, sHoursLeft, sMinutesLeft, sSecondsLeft] =
+   /free dyno hours quota remaining this month\: (.*h)? (.*m)? (.*s)?/i.exec(
+    stoutHerokuStatus
+   );
+
+  const dateEndOfMonth = endOfMonth(new Date());
+  const dateLeftCalculateWithMiliseconds = new Date(
+   new Date().getTime() +
+    ms({
+     hours: Number.parseInt(sHoursLeft || 0),
+     minutes: Number.parseInt(sMinutesLeft || 0),
+     seconds: Number.parseInt(sSecondsLeft || 0),
+    })
+  );
+
+  const isOff = stoutHerokuStatus.indexOf(MATCH_STATUS_OFF) !== -1;
+
+  if (isOff) {
    result = await exec("heroku ps:scale worker=1");
    console.log("Start bot B-Joke");
-  }catch(e){
-   console.error(e);
-  }
- } else {
-  console.log(`Not found ${MATCH_STATUS_OFF} in ${stoutHerokuStatus}`);
-  try{
+  } else if(isAfter(dateEndOfMonth, dateLeftCalculateWithMiliseconds)) {
+   console.log(`Not found ${MATCH_STATUS_OFF} in ${stoutHerokuStatus}`);
    result = await exec("heroku ps:scale worker=0");
-   console.log("Stop bot B-Joke")
-  }catch(e){
-   console.error(e);
+   console.log("Stop bot B-Joke");
+  } else {
+   console.info({
+    dateEndOfMonth: dateEndOfMonth.toString(),
+    dateLeftCalculateWithMiliseconds:
+     dateLeftCalculateWithMiliseconds.toString(),
+    isAfter: isAfter(dateEndOfMonth, dateLeftCalculateWithMiliseconds)
+   });
+   console.info(
+    "No es necesario apagar el bot, todo estará bien si se queda encendido"
+   );
   }
+ } catch (e) {
+  console.error(e);
  }
- console.info(result);
-})()
+})();
