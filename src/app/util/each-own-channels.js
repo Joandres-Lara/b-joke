04b15d0f6171e13be14b3166fa6d043931afc314
Joan Andrés Lara Mora <joandres52752lm@gmail.com>
2023-03-26 23:04:01 -0600

    fix(each-own-channels): disable chanels

--- src/app/util/each-own-channels.js
+++ src/app/util/each-own-channels.js
@@ -1,8 +1,11 @@
 export default function eachOwnChannels(bot, callback) {
- [
-  "779580409884704770",
-  // "772296886089416727"
- ].forEach((id) => {
-  callback({ id });
- });
+ []
+  .concat(
+   process.env.NODE_ENV == "test"
+    ? ["779580409884704770", "772296886089416727"]
+    : []
+  )
+  .forEach((id) => {
+   callback({ id });
+  });
 }
